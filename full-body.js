b6f333d541908ac01cdf11c1fb8f8456da53ab17
Velazquick <gonezeleps@gmail.com>
2026-08-02 11:19:24 -0400

    Add Full Body workout preset

---
+++ full-body.js
@@ -0,0 +1,33 @@
+(() => {
+  const day = 'FullBody';
+  const names = [
+    'Seated Machine Chest Press',
+    'Dumbbell Shoulder Press',
+    'Lat Pulldown',
+    'Triceps Pushdown',
+    'Dumbbell Lateral Raise',
+    'Hack Squat',
+    'Leg Extension',
+    'Standing Calf Raise'
+  ];
+
+  DEFAULT_ROUTINES[day] = {
+    label: 'Full Body',
+    exercises: names
+  };
+
+  const existingIds = new Set(EXERCISES.filter(exercise => exercise.day === day).map(exercise => exercise.id));
+  names.forEach(name => {
+    const source = EXERCISES.find(exercise => exercise.name === name);
+    if (source && !existingIds.has(source.id)) {
+      EXERCISES.push({ ...source, day });
+      existingIds.add(source.id);
+    }
+  });
+
+  if (selectedDay === day) {
+    renderLibrary();
+  } else {
+    renderSelectors();
+  }
+})();
